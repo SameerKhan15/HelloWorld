3a2511c5057ca01697124dd0afaa74c62735eb32
Sameer Khan <sameer15@hotmail.com>
2023-11-21 23:09:03 -0600

    Cleaned up the code Added a new testcase

--- Algos/src/sliding/window/MaxSumWithContigiousElements.java
+++ Algos/src/sliding/window/MaxSumWithContigiousElements.java
@@ -4,7 +4,7 @@ public class MaxSumWithContigiousElements {
 	
 	public int getMaxSum(int[] array) throws Exception {
 		if (array == null || array.length == 0) {
-			throw new Exception("input array is null or empty");
+			throw new Exception("input array is null is  or empty");
 		}
 		
 		int maxSum=array[0];
@@ -38,29 +38,80 @@ public class MaxSumWithContigiousElements {
 		return maxSum;
 	}
 	
+	public void printArray(int[] array) {
+		StringBuilder str = new StringBuilder();
+		for (int a = 0 ; a < array.length ; a++) {
+			str.append(array[a]+" ");
+		}
+		
+		System.out.println(str.toString());
+	}
+	
 	public static void main(String args[]) {
 		MaxSumWithContigiousElements algo = new MaxSumWithContigiousElements();
 		
+		/* testcase1 */
 		try {
-			/* testcase1 */
 			int[] a1 = {0, 1, -4, 5, 6, -2, 2};
-			System.out.println(algo.getMaxSum(a1));
-			
-			/* testcase2 */
+			System.out.println("=======Testcase1 InputArray ======");
+			algo.printArray(a1);
+			System.out.println("=======Output ======");
+			System.out.println(algo.getMaxSum(a1)+"\n");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		/* testcase2 */
+		try {	
 			int[] a2 = {0, 10, -4, 5, 6, -2, 2};
+			System.out.println("=======Testcase2 InputArray ======");
+			algo.printArray(a2);
+			System.out.println("=======Output ======");
 			System.out.println(algo.getMaxSum(a2));
-			
-			/* testcase3 */
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		/* testcase3 */
+		try {
 			int[] a3 = {-1, 2, 3, 6, 9, -9, 10, 11, -2};
+			System.out.println("=======Testcase3 InputArray ======");
+			algo.printArray(a3);
+			System.out.println("=======Output ======");
 			System.out.println(algo.getMaxSum(a3));
-			
-			/* testcase4 */
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		/* testcase4 */
+		try {
 			int[] a4 = {-2};
+			System.out.println("=======Testcase4 InputArray ======");
+			algo.printArray(a4);
+			System.out.println("=======Output ======");
 			System.out.println(algo.getMaxSum(a4));
-			
-			/* testcase5*/
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		/* testcase5*/
+		try {
 			int[] a5 = {};
+			System.out.println("=======Testcase5 InputArray ======");
+			algo.printArray(a5);
+			System.out.println("=======Output ======");
 			System.out.println(algo.getMaxSum(a5));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		/* testcase6 */
+		try {
+			int[] a6 = {-1, 4, 3, 6, 9, -9, 10, 11, -2};
+			System.out.println("=======Testcase6 InputArray ======");
+			algo.printArray(a6);
+			System.out.println("=======Output ======");
+			System.out.println(algo.getMaxSum(a6));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
